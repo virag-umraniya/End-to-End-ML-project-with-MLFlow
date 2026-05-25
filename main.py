8d29a0159fae5b9353323ea28d43d81213c48240
Virag Umraniya <irdgvaiiy@gmail.com>
2026-05-25 14:35:04 +0530

    Model Trainer Added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from mlProject import logger
 from mlProject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -21,6 +23,27 @@ try:
     obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>Stage {STAGE_NAME} completed <<<<<\n\nx========{STAGE_NAME}=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========{STAGE_NAME}========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>>>Stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> Stage {STAGE_NAME} completed <<<<<<< \n\nx======={STAGE_NAME}========x")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+# Data Ingestion 
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
@@ -8,10 +9,26 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
-
+# Data Validation
 @dataclass(frozen=True)
 class DataValidationConfig:
     root_dir: Path
     STATUS_FILE: str
     unzip_data_dir: Path
     all_schema: dict
+
+# Data Transformation
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path # Path for storing output
+    data_path: Path # Path for taking data input
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: str
+    alpha: float
+    l1_ratio: float
+    target_column: str
