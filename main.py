c95321f02dc967613f7ef4db380c26da0bd331ec
Virag Umraniya <irdgvaiiy@gmail.com>
2026-05-25 11:03:12 +0530

    Data Validation Added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -8,7 +9,18 @@ try:
     logger.info(f">>> Stage {STAGE_NAME} started <<<")
     obj = DataIngestionTrainingPipeline()
     obj.main()
-    logger.info(f">>> Stage {STAGE_NAME} completed <<<\n\nx==============================x")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    logger.info(f">>> Stage {STAGE_NAME} completed <<<\n\nx=============={STAGE_NAME}==================x")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>Stage {STAGE_NAME} started")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>Stage {STAGE_NAME} completed <<<<<\n\nx========{STAGE_NAME}=========x")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/mlProject/pipeline/stage01_data_ingestion.py
+++ src/mlProject/pipeline/stage01_data_ingestion.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         logger.info(f">>> Stage {STAGE_NAME} started <<<")
         obj = DataIngestionTrainingPipeline()
         obj.main()
-        logger.info(f">>> Stage {STAGE_NAME} completed <<<\n\nx======x")
+        logger.info(f">>> Stage {STAGE_NAME} completed <<<\n\nx=====STAGE_NAME=====x")
     except Exception as e:
         logger.exception(e)
         raise e
